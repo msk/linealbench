9030b3d1b2abfa5f615d252718f07fe3adb350b3
Min Kim <msk@dolbo.net>
2018-10-30 23:08:53 -0700

    Compare results using bench_functions()

--- benches/vector.rs
+++ benches/vector.rs
@@ -4,7 +4,7 @@ extern crate nalgebra;
 extern crate ndarray;
 extern crate rand;
 
-use criterion::Criterion;
+use criterion::{Criterion, Fun};
 use rand::distributions::Standard;
 use rand::{IsaacRng, Rng, SeedableRng};
 
@@ -15,17 +15,20 @@ fn ddot(c: &mut Criterion) {
 
     let xtmp = x.clone();
     let ytmp = y.clone();
-    c.bench_function("ddot_rblas", move |b| {
+    let ddot_rblas = Fun::new("RBLAS", move |b, _| {
         b.iter(|| rblas::Dot::dot(&xtmp, &ytmp))
     });
 
     let xtmp = nalgebra::DVector::from_iterator(x.len(), x.iter().cloned());
     let ytmp = nalgebra::DVector::from_iterator(x.len(), x.iter().cloned());
-    c.bench_function("ddot_nalgebra", move |b| b.iter(|| xtmp.dot(&ytmp)));
+    let ddot_nalgebra = Fun::new("nalgebra", move |b, _| b.iter(|| xtmp.dot(&ytmp)));
 
     let xtmp = ndarray::arr1(&x);
     let ytmp = ndarray::arr1(&y);
-    c.bench_function("ddot_ndarray", move |b| b.iter(|| xtmp.dot(&ytmp)));
+    let ddot_ndarray = Fun::new("ndarray", move |b, _| b.iter(|| xtmp.dot(&ytmp)));
+
+    let ddot_funs = vec![ddot_rblas, ddot_nalgebra, ddot_ndarray];
+    c.bench_functions("ddot", ddot_funs, 0);
 }
 
 criterion_group!(vector, ddot);

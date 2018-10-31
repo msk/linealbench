e4036103a5d0f8cb7f1281d8daab9de22ba830b0
Min Kim <msk@dolbo.net>
2018-10-30 23:26:16 -0700

    Parameterize benchmark tests

--- benches/vector.rs
+++ benches/vector.rs
@@ -15,20 +15,28 @@ fn ddot(c: &mut Criterion) {
 
     let xtmp = x.clone();
     let ytmp = y.clone();
-    let ddot_rblas = Fun::new("RBLAS", move |b, _| {
-        b.iter(|| rblas::Dot::dot(&xtmp, &ytmp))
+    let ddot_rblas = Fun::new("RBLAS", move |b, &i| {
+        b.iter(|| rblas::Dot::dot(&xtmp[..i], &ytmp[..i]))
     });
 
-    let xtmp = nalgebra::DVector::from_iterator(x.len(), x.iter().cloned());
-    let ytmp = nalgebra::DVector::from_iterator(x.len(), x.iter().cloned());
-    let ddot_nalgebra = Fun::new("nalgebra", move |b, _| b.iter(|| xtmp.dot(&ytmp)));
+    let xtmp = x.clone();
+    let ytmp = y.clone();
+    let ddot_nalgebra = Fun::new("nalgebra", move |b, &i| {
+        let x = nalgebra::DVector::from_iterator(i, xtmp[..i].iter().cloned());
+        let y = nalgebra::DVector::from_iterator(i, ytmp[..i].iter().cloned());
+        b.iter(|| x.dot(&y))
+    });
 
-    let xtmp = ndarray::arr1(&x);
-    let ytmp = ndarray::arr1(&y);
-    let ddot_ndarray = Fun::new("ndarray", move |b, _| b.iter(|| xtmp.dot(&ytmp)));
+    let xtmp = x.clone();
+    let ytmp = y.clone();
+    let ddot_ndarray = Fun::new("ndarray", move |b, &i| {
+        let x = ndarray::arr1(&xtmp[..i]);
+        let y = ndarray::arr1(&ytmp[..i]);
+        b.iter(|| x.dot(&y))
+    });
 
     let ddot_funs = vec![ddot_rblas, ddot_nalgebra, ddot_ndarray];
-    c.bench_functions("ddot", ddot_funs, 0);
+    c.bench_functions("ddot", ddot_funs, 100);
 }
 
 criterion_group!(vector, ddot);

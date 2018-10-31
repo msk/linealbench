89fbbc83c18c60dc5c3afe119ea541107cab69cf
Min Kim <msk@dolbo.net>
2018-10-30 23:44:29 -0700

    Produce a line graph

--- benches/vector.rs
+++ benches/vector.rs
@@ -4,7 +4,7 @@ extern crate nalgebra;
 extern crate ndarray;
 extern crate rand;
 
-use criterion::{Criterion, Fun};
+use criterion::{Criterion, ParameterizedBenchmark};
 use rand::distributions::Standard;
 use rand::{IsaacRng, Rng, SeedableRng};
 
@@ -13,30 +13,25 @@ fn ddot(c: &mut Criterion) {
     let x: Vec<f64> = rng.sample_iter(&Standard).take(1000).collect();
     let y: Vec<f64> = rng.sample_iter(&Standard).take(1000).collect();
 
-    let xtmp = x.clone();
-    let ytmp = y.clone();
-    let ddot_rblas = Fun::new("RBLAS", move |b, &i| {
-        b.iter(|| rblas::Dot::dot(&xtmp[..i], &ytmp[..i]))
-    });
+    let params = vec![10, 500, 1000];
+    let (x_rblas, y_rblas) = (x.clone(), y.clone());
+    let (x_nalgebra, y_nalgebra) = (x.clone(), y.clone());
+    let (x_ndarray, y_ndarray) = (x.clone(), y.clone());
 
-    let xtmp = x.clone();
-    let ytmp = y.clone();
-    let ddot_nalgebra = Fun::new("nalgebra", move |b, &i| {
-        let x = nalgebra::DVector::from_iterator(i, xtmp[..i].iter().cloned());
-        let y = nalgebra::DVector::from_iterator(i, ytmp[..i].iter().cloned());
+    let benchmark = ParameterizedBenchmark::new(
+        "RBLAS",
+        move |b, &i| b.iter(|| rblas::Dot::dot(&x_rblas[..i], &y_rblas[..i])),
+        params,
+    ).with_function("nalgebra", move |b, &i| {
+        let x = nalgebra::DVector::from_iterator(i, x_nalgebra[..i].iter().cloned());
+        let y = nalgebra::DVector::from_iterator(i, y_nalgebra[..i].iter().cloned());
+        b.iter(|| x.dot(&y))
+    }).with_function("ndarray", move |b, &i| {
+        let x = ndarray::arr1(&x_ndarray[..i]);
+        let y = ndarray::arr1(&y_ndarray[..i]);
         b.iter(|| x.dot(&y))
     });
-
-    let xtmp = x.clone();
-    let ytmp = y.clone();
-    let ddot_ndarray = Fun::new("ndarray", move |b, &i| {
-        let x = ndarray::arr1(&xtmp[..i]);
-        let y = ndarray::arr1(&ytmp[..i]);
-        b.iter(|| x.dot(&y))
-    });
-
-    let ddot_funs = vec![ddot_rblas, ddot_nalgebra, ddot_ndarray];
-    c.bench_functions("ddot", ddot_funs, 100);
+    c.bench("ddot", benchmark);
 }
 
 criterion_group!(vector, ddot);
